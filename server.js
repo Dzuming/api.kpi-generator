8ae25e5c060553f5c03069813b1e8a2d26137d86
dpoliszak <dawid.poliszak@lptg.pl>
2017-09-05 09:34:31 +0200

    Beautify code

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 import express from 'express';
-import {graphqlExpress, graphiqlExpress} from 'apollo-server-express';
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import bodyParser from 'body-parser';
 import db from './models';
-var models = require('./models');
 import schema from './data/schema';
 
 const GRAPHQL_PORT = 3000;
 
 const graphQLServer = express();
 
-graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({schema}));
-graphQLServer.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}));
+graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 db
   .sequelize
   .sync()
-  .then(function () {
-    var server = graphQLServer.listen(GRAPHQL_PORT, function () {
+  .then(() => {
+    const server = graphQLServer.listen(GRAPHQL_PORT, () => {
       console.log('Express server listening on port ' + server.address().port);
     });
   });
